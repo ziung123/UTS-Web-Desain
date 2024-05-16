1066ba194c445f1dcc26d4e66737115796092717
ziung123 <118096591+ziung123@users.noreply.github.com>
2024-05-16 08:11:52 +0700

    Add files via upload

---
+++ Script UTS.js
@@ -0,0 +1,8 @@
+function showMoreInfo() {
+    var moreInfo = document.getElementById("moreInfo");
+    if (moreInfo.style.display === "none") {
+        moreInfo.style.display = "block";
+    } else {
+        moreInfo.style.display = "none";
+    }
+}
